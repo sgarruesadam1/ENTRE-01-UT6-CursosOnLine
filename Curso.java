9b7fab5d79fbf61ac38d1b6f9bdc80b4876f32ca
sgarruesadam1 <sgarruesadam1@gmail.com>
2020-02-20 22:51:55 +0100

    Casi acabado

--- Curso.java
+++ Curso.java
@@ -4,100 +4,116 @@ import java.time.format.DateTimeFormatter;
 /**
  *  Modela un curso ofrecido por la plataforma
  *  on line
+ *  @author Sergio Garrués Aizcorbe
  */
 public class Curso
 {
 
-	private final String ESPACIO = " ";
-	private String nombre;
-	private LocalDate fecha;
-	private Nivel nivel;
+    private final String ESPACIO = " ";
+    private String nombre;
+    private LocalDate fecha;
+    private Nivel nivel;
 
-	/**
-	 * Constructor  
-	 */
-	public Curso(String nombre, LocalDate fecha, Nivel nivel) {
+    /**
+     * Constructor  
+     */
+    public Curso(String nombre, LocalDate fecha, Nivel nivel) {
 
-		this.nombre = capitalizarNombre(nombre);
-		this.fecha = fecha;
-		this.nivel = nivel;
+        this.nombre = capitalizarNombre(nombre);
+        this.fecha = fecha;
+        this.nivel = nivel;
 
-	}
+    }
 
-	/**
-	 * Dado un nombre de curso lo devuelve capitalizado, es decir,
-	 * cada una de las palabras del nombre empiezan por mayúscula y el
-	 * resto en minúsculas. Se eliminan espacios antes y después del nombre
-	 * (asumimos entre palabras un único espacio)
-	 * 
-	 * Si nombre = " sql essential training " devuelve "Sql Essential Training"
-	 */
-	private String capitalizarNombre(String nombre) {
+    /**
+     * Dado un nombre de curso lo devuelve capitalizado, es decir,
+     * cada una de las palabras del nombre empiezan por mayúscula y el
+     * resto en minúsculas. Se eliminan espacios antes y después del nombre
+     * (asumimos entre palabras un único espacio)
+     * 
+     * Si nombre = " sql essential training " devuelve "Sql Essential Training"
+     */
+    private String capitalizarNombre(String nombre) {
+        
+        String[] str = nombre.split(ESPACIO);
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < str.length; i++){
+            if(!str[i].isEmpty()){
+            sb.append(str[i].trim().substring(0,1).toUpperCase() + str[i].substring(1) + ESPACIO);
+        }
+       }
+         
+        return sb.toString().trim();
 
-		 
-		return "";
+    }
 
-	}
+    /**
+     *  accesor para el nombre
+     */
+    public String getNombre() {
 
-	/**
-	 *  accesor para el nombre
-	 */
-	public String getNombre() {
+        return this.nombre;
+    }
 
-		return this.nombre;
-	}
+    /**
+     *  accesor para el nivel
+     */
+    public Nivel getNivel() {
 
-	/**
-	 *  accesor para el nivel
-	 */
-	public Nivel getNivel() {
+        return this.nivel;
+    }
 
-		return this.nivel;
-	}
+    /**
+     *  accesor para la fecha 
+     */
+    public LocalDate getFecha() {
 
-	/**
-	 *  accesor para la fecha 
-	 */
-	public LocalDate getFecha() {
+        return fecha;
+    }
 
-		return fecha;
-	}
+    /**
+     *  accesor para la fecha formateada
+     *  Se devuelve la fecha de publicación del curso como una cadena formateada
+     *  de la forma "día nombre-mes año"  Ej. "03 diciembre 2019" 
+     */
+    public String getFechaFormateada() {
+        String[] str = fecha.toString().trim().split("-");
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(fecha.format(DateTimeFormatter.ofPattern("dd MMMM yyyy")));
+        LocalDate fecha1 = LocalDate.of(fecha.getYear(), fecha.getMonth(), fecha.getDayOfMonth());
+        DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        String strFecha1 = fecha1.format(formateador);
+        
+        return sb.toString();
+    }
 
-	/**
-	 *  accesor para la fecha formateada
-	 *  Se devuelve la fecha de publicación del curso como una cadena formateada
-	 *  de la forma "día nombre-mes año"  Ej. "03 diciembre 2019" 
-	 */
-	public String getFechaFormateada() {
+    /**
+     *  Representación textual del curso
+     */
+    public String toString() {
 
-		return "";
-	}
+        String str = String.format("%20s: %s\n", "Nombre", this.nombre);
+        str += String.format("%20s: %s\n%20s: %s\n", "Publicado desde",
+                        this.getFechaFormateada(),
+                        "Nivel", this.nivel.toString());
 
-	/**
-	 *  Representación textual del curso
-	 */
-	public String toString() {
+        str += "\n";
+        return str;
+    }
 
-		String str = String.format("%20s: %s\n", "Nombre", this.nombre);
-		str += String.format("%20s: %s\n%20s: %s\n", "Publicado desde",
-		                this.getFechaFormateada(),
-		                "Nivel", this.nivel.toString());
+    /**
+     *  Probar la clase Curso
+     */
+    public static void main(String[] args) {
 
-		str += "\n";
-		return str;
-	}
-
-	/**
-	 *  Probar la clase Curso
-	 */
-	public static void main(String[] args) {
-
-		Curso curso1 = new Curso("  sql essential training ",
-		                LocalDate.of(2019, 12, 3), Nivel.PRINCIPIANTE);
-		System.out.println(curso1.toString());
-		Curso curso2 = new Curso(" wordpress PLugin development   ",
-		                LocalDate.of(2011, 11, 30), Nivel.INTERMEDIO);
-		System.out.println(curso2.toString());
-	}
+        Curso curso1 = new Curso("  sql essential training ",
+                        LocalDate.of(2019, 12, 3), Nivel.PRINCIPIANTE);
+        System.out.println(curso1.toString());
+        Curso curso2 = new Curso(" wordpress PLugin development   ",
+                        LocalDate.of(2011, 11, 30), Nivel.INTERMEDIO);
+        System.out.println(curso2.toString());
+    }
 
 }
--- PlataformaCursos.java
+++ PlataformaCursos.java
@@ -17,182 +17,264 @@ import java.util.TreeSet;
  * 
  * Las claves en el map se recuperan en orden alfabético y 
  * se guardan siempre en mayúsculas
+ *
+ * @author Sergio Garrués Aizcorbe
  */
 public class PlataformaCursos
 {
 
-	private final String ESPACIO = " ";
-	private final String SEPARADOR = ":";
-	private TreeMap<String, ArrayList<Curso>> plataforma;
+    private final String ESPACIO = " ";
+    private final String SEPARADOR = ":";
+    private TreeMap<String, ArrayList<Curso>> plataforma;
 
-	/**
-	 * Constructor  
-	 */
-	public PlataformaCursos() {
+    /**
+     * Constructor  
+     */
+    public PlataformaCursos() {
 
-		plataforma = new TreeMap<>();
+        plataforma = new TreeMap<>();
 
-	}
+    }
 
-	/**
-	 * añadir un nuevo curso al map en la categoría indicada
-	 * Si ya existe la categoría se añade en ella el nuevo curso
-	 * (al final de la lista)
-	 * En caso contrario se creará una nueva entrada en el map con
-	 * la nueva categoría y el curso que hay en ella
-	 * Las claves siempre se añaden en mayúsculas  
-	 *  
-	 */
-	public void addCurso(String categoria, Curso curso) {
+    /**
+     * añadir un nuevo curso al map en la categoría indicada
+     * Si ya existe la categoría se añade en ella el nuevo curso
+     * (al final de la lista)
+     * En caso contrario se creará una nueva entrada en el map con
+     * la nueva categoría y el curso que hay en ella
+     * Las claves siempre se añaden en mayúsculas  
+     *  
+     */
+    public void addCurso(String categoria, Curso curso) {
+         ArrayList<Curso> array = new ArrayList<>();
+        
+         if(!plataforma.containsKey(categoria.toUpperCase())){ 
+             array.add(curso);
+             plataforma.put(categoria.toUpperCase(), array);
+          }
+         else{
+             plataforma.get(categoria.toUpperCase()).add(curso);
+          }
 
-		 
+    }
 
-	}
+    /**
+     *  Devuelve la cantidad de cursos en la categoría indicada
+     *  Si no existe la categoría devuelve -1
+     *
+     */
+    public int totalCursosEn(String categoria) {
+        int total = -1;
+        
+        if(plataforma.containsKey(categoria.toUpperCase()))
+        {
+         total = plataforma.get(categoria.toUpperCase()).size();   
+        }
+     
+        return total;
+    }
 
-	/**
-	 *  Devuelve la cantidad de cursos en la categoría indicada
-	 *  Si no existe la categoría devuelve -1
-	 *
-	 */
-	public int totalCursosEn(String categoria) {
+    /**
+     * Representación textual de la plataforma (el map), cada categoría
+     * junto con el nº total de cursos que hay en ella y a continuación
+     * la relación de cursos en esa categoría (ver resultados de ejecución)
+     * 
+     * De forma eficiente ya que habrá muchas concatenaciones
+     * 
+     * Usar el conjunto de entradas y un iterador
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Set<String> keys = plataforma.keySet();
+        
+        
+        for(String clave : keys){
+            sb.append(clave + " (" + totalCursosEn(clave) + ")\n");
+            
+            for(Curso curso : plataforma.get(clave.toUpperCase()))
+            {
+            sb.append(curso.toString() + "\n\n");
+          }
+        }
+       
+         
+        return sb.toString();
 
-		 
-		return 0;
-	}
+    }
 
-	/**
-	 * Representación textual de la plataforma (el map), cada categoría
-	 * junto con el nº total de cursos que hay en ella y a continuación
-	 * la relación de cursos en esa categoría (ver resultados de ejecución)
-	 * 
-	 * De forma eficiente ya que habrá muchas concatenaciones
-	 * 
-	 * Usar el conjunto de entradas y un iterador
-	 */
-	public String toString() {
+    /**
+     * Mostrar la plataforma
+     */
+    public void escribir() {
 
-		 
-		return "";
+        System.out.println(this.toString());
+    }
 
-	}
+    /**
+     *  Lee de un fichero de texto la información de los cursos
+     *  En cada línea del fichero se guarda la información de un curso
+     *  con el formato "categoria:nombre:fecha publicacion:nivel"
+     *  
+     */
+    public void leerDeFichero() {
 
-	/**
-	 * Mostrar la plataforma
-	 */
-	public void escribir() {
+        Scanner sc = new Scanner(
+                        this.getClass().getResourceAsStream("/cursos.csv"));
+        while (sc.hasNextLine())  {
+            String lineaCurso = sc.nextLine().trim();
+            int p = lineaCurso.indexOf(SEPARADOR);
+            String categoria = lineaCurso.substring(0, p).trim();
+            Curso curso = obtenerCurso(lineaCurso.substring(p + 1));
+            this.addCurso(categoria, curso);
+        }
 
-		System.out.println(this.toString());
-	}
+    }
 
-	/**
-	 *  Lee de un fichero de texto la información de los cursos
-	 *  En cada línea del fichero se guarda la información de un curso
-	 *  con el formato "categoria:nombre:fecha publicacion:nivel"
-	 *  
-	 */
-	public void leerDeFichero() {
+    /**
+     *  Dado un String con los datos de un curso
+     *  obtiene y devuelve un objeto Curso
+     *
+     *  Ej. a partir de  "sql essential training: 3/12/2019 : principiante " 
+     *  obtiene el objeto Curso correspondiente
+     *  
+     *  Asumimos todos los valores correctos aunque puede haber 
+     *  espacios antes y después de cada dato
+     */
+    public Curso obtenerCurso(String lineaCurso) {
+        String[] str = lineaCurso.trim().split(SEPARADOR);
+        
+        LocalDate localDate1 = LocalDate.parse(str[1].trim(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        Curso curso = new Curso(str[0].trim(), localDate1, Nivel.valueOf(str[2].toUpperCase().trim()));
+         
+        return curso;
 
-		Scanner sc = new Scanner(
-		                this.getClass().getResourceAsStream("/cursos.csv"));
-		while (sc.hasNextLine())  {
-			String lineaCurso = sc.nextLine().trim();
-			int p = lineaCurso.indexOf(SEPARADOR);
-			String categoria = lineaCurso.substring(0, p).trim();
-			Curso curso = obtenerCurso(lineaCurso.substring(p + 1));
-			this.addCurso(categoria, curso);
-		}
+    }
 
-	}
+    /**
+     * devuelve un nuevo conjunto con los nombres de todas las categorías  
+     *  
+     */
+    public TreeSet<String> obtenerCategorias() {
+        TreeSet<String> tree = new TreeSet<>();
+        Set<String> claves = plataforma.keySet();
+        
+        for(String key : claves)
+        {
+            tree.add(key);
+        }
+        
+        return tree;
 
-	/**
-	 *  Dado un String con los datos de un curso
-	 *  obtiene y devuelve un objeto Curso
-	 *
-	 *  Ej. a partir de  "sql essential training: 3/12/2019 : principiante " 
-	 *  obtiene el objeto Curso correspondiente
-	 *  
-	 *  Asumimos todos los valores correctos aunque puede haber 
-	 *  espacios antes y después de cada dato
-	 */
-	private Curso obtenerCurso(String lineaCurso) {
+    }
 
-		 
-		return null;
+    /**
+     * borra de la plataforma los cursos de la categoría y nivel indicados
+     * Se devuelve un conjunto (importa el orden) con los nombres de los cursos borrados 
+     * 
+     * Asumimos que existe la categoría
+     *  
+     */
 
-	}
+    public   TreeSet<String>   borrarCursosDe(String categoria, Nivel nivel) {
+        TreeSet<String> tree = new TreeSet<>();
+        Set<String> claves = plataforma.keySet();
+        
+        for(String clave : claves){
+            if(clave.equalsIgnoreCase(categoria))
+            {
+            
+            for(Curso curso : plataforma.get(clave.toUpperCase()))
+            {
+            
+            
+                tree.add(curso.getNombre()); 
+                plataforma.get(clave).remove(curso);
+            }
+          }
+          
+        }
+        
+        
+                
+        for(String clave : claves){
+            
+            
+            for(Curso curso : plataforma.get(clave.toUpperCase()))
+            {
+            if(curso.getNivel().compareTo(nivel) > 0)
+            {   
+                tree.add(curso.getNombre());
+                plataforma.get(clave).remove(curso);
+            }
+          }
+        }
+          
+        return tree;
 
-	/**
-	 * devuelve un nuevo conjunto con los nombres de todas las categorías  
-	 *  
-	 */
-	public TreeSet<String> obtenerCategorias() {
+    }
 
-		return null;
+    /**
+      *   Devuelve el nombre del curso más antiguo en la
+      *   plataforma (el primero publicado)
+      */
 
-	}
+    public String cursoMasAntiguo() {
+        String antiguo = "";
+         LocalDate fecha = LocalDate.now();
+        
+        Set<String> keys = plataforma.keySet();
+                
+        for(String clave : keys){
+            
+            
+            for(Curso curso : plataforma.get(clave.toUpperCase()))
+            {
+            if(fecha.compareTo(curso.getFecha()) > 0)
+            {
+                fecha = curso.getFecha();
+                antiguo = curso.getNombre();
+            }
+          }
+        }
 
-	/**
-	 * borra de la plataforma los cursos de la categoría y nivel indicados
-	 * Se devuelve un conjunto (importa el orden) con los nombres de los cursos borrados 
-	 * 
-	 * Asumimos que existe la categoría
-	 *  
-	 */
+        return antiguo;
+    }
 
-	public      borrarCursosDe(String categoria, Nivel nivel) {
+    /**
+     *  
+     */
+    public static void main(String[] args) {
 
-		 
-		return null;
+        PlataformaCursos plataforma = new PlataformaCursos();
+        plataforma.leerDeFichero();
+        plataforma.escribir();
 
-	}
+        System.out.println(
+                        "Curso más antiguo: " + plataforma.cursoMasAntiguo()
+                                        + "\n");
 
-	/**
-	  *   Devuelve el nombre del curso más antiguo en la
-	  *   plataforma (el primero publicado)
-	  */
+        String categoria = "bases de datos";
+        Nivel nivel = Nivel.AVANZADO;
+        System.out.println("------------------");
+        System.out.println(
+                        "Borrando cursos de " + categoria.toUpperCase()
+                                        + " y nivel "
+                                        + nivel);
+        TreeSet<String> borrados = plataforma.borrarCursosDe(categoria, nivel);
 
-	public String cursoMasAntiguo() {
+        System.out.println("Borrados " + " = " + borrados.toString() + "\n");
+        categoria = "cms";
+        nivel = Nivel.INTERMEDIO;
+        System.out.println(
+                        "Borrando cursos de " + categoria.toUpperCase()
+                                        + " y nivel "
+                                        + nivel);
+        borrados = plataforma.borrarCursosDe(categoria, nivel);
+        System.out.println("Borrados " + " = " + borrados.toString() + "\n");
+        System.out.println("------------------\n");
+        System.out.println(
+                        "Después de borrar ....");
+        plataforma.escribir();
 
-		 
-
-		return "";
-	}
-
-	/**
-	 *  
-	 */
-	public static void main(String[] args) {
-
-		PlataformaCursos plataforma = new PlataformaCursos();
-		plataforma.leerDeFichero();
-		plataforma.escribir();
-
-		System.out.println(
-		                "Curso más antiguo: " + plataforma.cursoMasAntiguo()
-		                                + "\n");
-
-		String categoria = "bases de datos";
-		Nivel nivel = Nivel.AVANZADO;
-		System.out.println("------------------");
-		System.out.println(
-		                "Borrando cursos de " + categoria.toUpperCase()
-		                                + " y nivel "
-		                                + nivel);
-		TreeSet<String> borrados = plataforma.borrarCursosDe(categoria, nivel);
-
-		System.out.println("Borrados " + " = " + borrados.toString() + "\n");
-		categoria = "cms";
-		nivel = Nivel.INTERMEDIO;
-		System.out.println(
-		                "Borrando cursos de " + categoria.toUpperCase()
-		                                + " y nivel "
-		                                + nivel);
-		borrados = plataforma.borrarCursosDe(categoria, nivel);
-		System.out.println("Borrados " + " = " + borrados.toString() + "\n");
-		System.out.println("------------------\n");
-		System.out.println(
-		                "Después de borrar ....");
-		plataforma.escribir();
-
-	}
+    }
 }
